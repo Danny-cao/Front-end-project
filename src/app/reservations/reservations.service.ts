cce56cbaf3b2c473250191081415ae306eaa1bc8
Danny Cao <danny.dc.cao@gmail.com>
2019-10-06 15:15:56 +0200

    fixed dependencies before merge

--- src/app/reservations/reservations.service.ts
+++ src/app/reservations/reservations.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Reservation } from './reservationModel/Reservation';
 import { Observable } from 'rxjs';
 
@@ -30,7 +30,5 @@ export class ReservationsService {
       status: 'geannuleerd'
     });
   }
-
-
 }
 
